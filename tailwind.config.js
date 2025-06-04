312381063c01670f3e5cc66730fcf6fbe7d7a75e
Byeongju Park <beyondzux@gmail.com>
2025-06-04 21:52:07 +0900

    chore: Tailwind CSS 디자인 토큰 반영
    - 텍스트 및 배경 색상 주석 수정
    - 새로운 폰트 사이즈 추가 (디스플레이, 헤딩, 바디)
    - 박스 그림자 및 테두리 반경 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,7 +32,7 @@ module.exports = {
           50: "#05F86F",
           80: "#03632D",
         },
-        // Color System : Text
+        // Text
         "text-primary": "#F86B1F",
         "text-strong": "#0F0F0F",
         "text-bold": "#0F0F0F",
@@ -41,19 +41,91 @@ module.exports = {
         "text-white-default": "#FEFEFE",
         "text-white-weak": "#E3E3E3",
         "background-default": "#F0F0F0",
-        // Color System : Background / Surface
+        // Background / Surface
         "surface-white": "#FEFEFE",
         "surface-gray": "#515151",
       },
       fontSize: {
-        // TODO: 폰트 사이즈 추가
+        // Display
+        "display-large": [
+          "100px",
+          { lineHeight: "auto", letterSpacing: "1px" },
+        ],
+        "display-large-m": [
+          "44px",
+          { lineHeight: "auto", letterSpacing: "1px" },
+        ],
+        "display-medium": [
+          "64px",
+          { lineHeight: "auto", letterSpacing: "1px" },
+        ],
+        "display-medium-m": [
+          "32px",
+          { lineHeight: "auto", letterSpacing: "1px" },
+        ],
+        "display-medium-bold": [
+          "64px",
+          { lineHeight: "auto", letterSpacing: "1px" },
+        ],
+        "display-medium-bold-m": [
+          "32px",
+          { lineHeight: "auto", letterSpacing: "1px" },
+        ],
+        "display-small": ["36px", { lineHeight: "auto", letterSpacing: "1px" }],
+        "display-small-m": [
+          "28px",
+          { lineHeight: "auto", letterSpacing: "1px" },
+        ],
+        // Heading
+        "heading-large": ["40px", { lineHeight: "auto", letterSpacing: "1px" }],
+        "heading-large-m": [
+          "28px",
+          { lineHeight: "auto", letterSpacing: "1px" },
+        ],
+        "heading-medium": [
+          "24px",
+          { lineHeight: "auto", letterSpacing: "1px" },
+        ],
+        "heading-medium-m": [
+          "22px",
+          { lineHeight: "auto", letterSpacing: "1px" },
+        ],
+        "heading-small": ["14px", { lineHeight: "auto", letterSpacing: "1px" }],
+        "heading-small-m": [
+          "14px",
+          { lineHeight: "auto", letterSpacing: "1px" },
+        ],
+        // Body
+        "body-large": ["19px", { lineHeight: "auto", letterSpacing: "0px" }],
+        "body-large-bold": [
+          "19px",
+          { lineHeight: "auto", letterSpacing: "0px" },
+        ],
+        "body-medium": ["15px", { lineHeight: "auto", letterSpacing: "0px" }],
+        "body-medium-bold": [
+          "15px",
+          { lineHeight: "auto", letterSpacing: "0px" },
+        ],
+        "body-small": ["10px", { lineHeight: "auto", letterSpacing: "0px" }],
+        "body-small-bold": [
+          "10px",
+          { lineHeight: "auto", letterSpacing: "0px" },
+        ],
       },
       fontWeight: {
         regular: "400",
         bold: "700",
       },
       boxShadow: {
-        header: "0px 8px 32px 0px #D7D7D7",
+        header: [
+          "0px 8px 32px 0px #D7D7D7",
+          "0px 4px 4px 0px rgba(0,0,0,0.25)",
+        ],
+        body: "0px -4px 16px 0px #D7D7D7",
+      },
+      borderRadius: {
+        "rounded-15": "15px",
+        "rounded-40": "40px",
       },
     },
   },
